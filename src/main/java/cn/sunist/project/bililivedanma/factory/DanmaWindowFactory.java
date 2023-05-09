454efbbc54571b121429a1b8b1a7516b42a68d66
sunist-c <sunist.c@outlook.com>
2023-05-09 12:44:13 +0800

    fix deprecated class ContentFactory.SERVICE

--- src/main/java/cn/sunist/project/bililivedanma/factory/DanmaWindowFactory.java
+++ src/main/java/cn/sunist/project/bililivedanma/factory/DanmaWindowFactory.java
@@ -1,6 +1,7 @@
 package cn.sunist.project.bililivedanma.factory;
 
 import cn.sunist.project.bililivedanma.ui.Danmaku;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
@@ -11,9 +12,13 @@ import org.jetbrains.annotations.NotNull;
 public class DanmaWindowFactory implements ToolWindowFactory {
     private final Danmaku danmaku = new Danmaku();
 
+    private final ContentFactory getInstance() {
+        return ApplicationManager.getApplication().getService(ContentFactory.class);
+    }
+
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        ContentFactory contentFactory = getInstance();
         Content content = contentFactory.createContent(danmaku.getComponent(), "", false);
         toolWindow.getContentManager().addContent(content);
     }
